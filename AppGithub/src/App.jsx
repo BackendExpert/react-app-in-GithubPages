575ce16358b448e4cbc2f35a5be3ed82244308cb
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-29 18:26:08 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- AppGithub/src/App.jsx
+++ AppGithub/src/App.jsx
@@ -1,7 +1,12 @@
+import { BrowserRouter, Route, Routes,  } from "react-router-dom";
+import Homepage from "./comportments/HomePage/Homepage";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
